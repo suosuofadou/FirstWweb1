0065677401e3d3df0ee3c3bce0a8b09d583cc7c0
suosuofadou <suosuofadou@163.com>
2019-10-16 11:27:35 +0800

    第er次

---
+++ src/Demo.java
@@ -0,0 +1,29 @@
+public class Demo {
+    private  String username;
+    private  String password;
+    private  int phone;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
